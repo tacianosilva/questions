6fdee55dbbd343495da444db095b7154064999ef
Taciano Morais Silva <tacianosilva@gmail.com>
2013-11-08 11:08:23 -0300

    Classe Main para testes com pequenas modificações!

--- src/main/java/br/ufrn/ceres/bsi/questions/Main.java
+++ src/main/java/br/ufrn/ceres/bsi/questions/Main.java
@@ -1,35 +1,61 @@
 package br.ufrn.ceres.bsi.questions;
 
+import java.util.List;
+
+import br.ufrn.ceres.bsi.questions.dao.UserService;
+import br.ufrn.ceres.bsi.questions.dao.util.JPAUtil;
+import br.ufrn.ceres.bsi.questions.model.Endereco;
 import br.ufrn.ceres.bsi.questions.model.Alternativa;
 import br.ufrn.ceres.bsi.questions.model.Questao;
+import br.ufrn.ceres.bsi.questions.model.Usuario;
+
 
 public class Main {
 
     /**
+     * Método main para testar os conceitos.
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
-        //Criando questões
-        Questao q1 = new Questao("q1");
+        Endereco endereco = new Endereco();
+        endereco.setCity("Caicó");
+        endereco.setCountry("Brasil");
+        endereco.setStreet("Presidente Kennedy");
+        endereco.setSuburb("Acampamento");
 
-        //Criando as alternativas
-        Alternativa a1 = new Alternativa("a1");
-        Alternativa a2 = new Alternativa("a2");
-        Alternativa a3 = new Alternativa("a3");
-        Alternativa a4 = new Alternativa("a4");
-        Alternativa a5 = new Alternativa("a4");
+        Usuario user1 = new Usuario();
+        user1.setUsername("tacianosilva");
+        user1.setFirstname("Taciano");
+        user1.setLastname("Silva");
+        user1.setEmail("tacianosilva@gmail.com");
+        user1.setPassword("12345");
+        user1.setAddress(endereco);
 
-        //Adicionando alternativas nas questões
-        q1.addAlternativa(a1);
-        q1.addAlternativa(a2);
-        q1.addAlternativa(a3);
-        q1.addAlternativa(a4);
-        q1.addAlternativa(a5);
-        q1.addAlternativaCorreta(a3);
-        q1.addAlternativaCorreta(a5);
+        Usuario user2 = new Usuario();
+        user2.setUsername("xuxa");
+        user2.setFirstname("Xuxa");
+        user2.setLastname("????");
+        user2.setEmail("xuxa@gmail.com");
+        user2.setPassword("12345");
+        user2.setAddress(endereco);
+
+        UserService service = new UserService(JPAUtil.EMF);
+        service.create(user1);
+        service.create(user2);
+
+        System.out.println(service.find(user1.getId()));
+        System.out.println(service.find(user2.getId()));
+
+        //service.delete(user1.getId());
+
+        System.out.println(service.find(user1.getId()));
+        System.out.println(service.find(user2.getId()));
+
+        List<Usuario> lista = service.findByQuery("SELECT u FROM Usuario u");
+        for (Usuario usuario : lista) {
+            System.out.println(usuario);
+        }
 
     }
-
 }
